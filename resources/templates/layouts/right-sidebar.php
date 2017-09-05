2c8e719a8612ccafd0ecc7c135632d6e2307259f
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-09-05 19:05:56 +0300

    initial add layouts

---
+++ resources/templates/layouts/right-sidebar.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <main id="main" class="site-main col-md-9">
+            <?php get_template_part('resources/templates/posts/single'); ?>
+        </main><!-- #main -->
+        <?php get_template_part('resources/templates/sidebars/right'); ?>
+    </div><!-- #primary -->
+</div><!-- #primary -->
--- single.php
+++ single.php
@@ -14,16 +14,7 @@ get_header(); ?>
             get_template_part('resources/templates/posts/header/banner');
         }
         ?>
-        <div class="container">
-            <div class="row">
-                <main id="main" class="site-main col-md-9">
-                    <?php get_template_part('resources/templates/posts/single'); ?>
-                </main><!-- #main -->
-                <?php get_template_part('resources/templates/sidebars/right'); ?>
-            </div><!-- #primary -->
-        </div><!-- #primary -->
+        <?php get_template_part('resources/templates/layouts/right-sidebar'); ?>
     </div><!-- #primary -->
-
 <?php
-get_sidebar();
 get_footer();
