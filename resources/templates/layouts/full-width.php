7715ed4585106f1747c8321ed06362894e242b03
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-09-13 13:20:24 +0300

    update full width  template

--- resources/templates/layouts/full-width.php
+++ resources/templates/layouts/full-width.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container-fluid">
     <div class="row">
         <main id="main" class="site-main col-md-12">
             <?php get_template_part('resources/templates/posts/single'); ?>
