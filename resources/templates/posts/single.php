e88f2eb5d46923d80645bd6c699e3b977c6cd5a1
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-09-25 21:56:58 +0300

    remove post navigation

--- resources/templates/posts/single.php
+++ resources/templates/posts/single.php
@@ -3,7 +3,7 @@ while (have_posts()) : the_post();
 
     get_template_part('resources/templates/posts/content/content', 'single');
 
-    the_post_navigation();
+//    the_post_navigation();
 
     // If comments are open or we have at least one comment, load up the comment template.
     if (comments_open() || get_comments_number()) :
