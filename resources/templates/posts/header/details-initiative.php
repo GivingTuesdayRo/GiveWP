09f46a05c6c3ec0f158fe4bf98b33d81ef47a364
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-11-01 10:37:28 +0200

    update initiative page

--- resources/templates/posts/header/details-initiative.php
+++ resources/templates/posts/header/details-initiative.php
@@ -2,11 +2,17 @@
 $pageType = get_post_type();
 $pageId = get_the_ID();
 
-$initiativeFields = ['initiative_name', 'organization_name', 'initiative_date', 'initiative_location'];
+$initiativeFields = [
+    'organization_name' => 'initiative_organization_organization_name',
+    'initiative_date' => 'initiative_options_initiative_date',
+    'initiative_address' => 'initiative_options_initiative_address',
+    'initiative_city' => 'initiative_options_initiative_city',
+    'initiative_county' => 'initiative_options_initiative_county',
+];
 $initiativeValues = [];
 
-foreach ($initiativeFields as $field) {
-    $metaName = 'givewp_'.$pageType.'_options_'.$field;
+foreach ($initiativeFields as $field => $optionName) {
+    $metaName = 'givewp_'.$optionName;
     $value = null;
     if (metadata_exists('post', $pageId, $metaName)) {
         $value = get_post_meta($pageId, $metaName, true);
@@ -17,14 +23,41 @@ foreach ($initiativeFields as $field) {
 <table class="table">
     <tr>
         <td style="width: 50px; word-wrap: break-word;"><strong>Ce:</strong></td>
-        <td><?php echo $initiativeValues['initiative_name'];?> </td>
+        <td>
+            <?php
+            $return = [];
+            $terms = get_the_terms($pageId, 'initiative-type');
+            foreach ($terms as $term) {
+                $return[] = $term->name;
+            }
+            echo implode(', ', $return);
+            ?>
+        </td>
         <td style="width: 50px; word-wrap: break-word;"><strong>Cine:</strong></td>
-        <td><?php echo $initiativeValues['organization_name'];?> </td>
+        <td>
+            <strong>
+                <?php
+                $return = [];
+                $terms = get_the_terms($pageId, 'initiator-type');
+                foreach ($terms as $term) {
+                    $return[] = $term->name;
+                }
+                echo implode(', ', $return);
+                ?>
+            </strong>
+            <?php echo $initiativeValues['organization_name']; ?>
+        </td>
     </tr>
     <tr>
         <td><strong>Cand:</strong></td>
-        <td><?php echo $initiativeValues['initiative_date'];?> </td>
+        <td>
+            <?php echo $initiativeValues['initiative_date']; ?>
+        </td>
         <td><strong>Unde:</strong></td>
-        <td><?php echo $initiativeValues['initiative_location'];?> </td>
+        <td>
+            <?php echo $initiativeValues['initiative_address']; ?>
+            <?php echo $initiativeValues['initiative_city']; ?>
+            <?php echo $initiativeValues['initiative_county']; ?>
+        </td>
     </tr>
 </table>
