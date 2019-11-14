c49ea7e54fb71cef1f69248575e1704c4448e37e
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-11-14 10:36:09 +0200

    update header theme

--- resources/templates/posts/header/header-regular.php
+++ resources/templates/posts/header/header-regular.php
@@ -2,7 +2,9 @@
     <div class="banner-overlay">
         <div class="banner-bar">
             <div class="container">
-                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                <div class="col-md-9 col-lg-8 pl-0">
+                    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                </div>
             </div>
         </div>
     </div>
--- resources/templates/posts/header/header.php
+++ resources/templates/posts/header/header.php
@@ -16,8 +16,10 @@ $coverStyle = PostMetadata::get($pageType, $pageId, PostMetadata::compileOptionN
 ?>
 <header class="entry-header entry-<?php echo $pageType ?> entry-header-<?php echo $coverStyle ?>">
     <?php
-    if (has_post_thumbnail() && $coverStyle != 'title') {
+    if (has_post_thumbnail() && $coverStyle == 'cover') {
         get_template_part('resources/templates/posts/header/header-cover');
+    } elseif (has_post_thumbnail() && $coverStyle == 'regular') {
+        get_template_part('resources/templates/posts/header/header-regular');
     } else {
         get_template_part('resources/templates/posts/header/header-title');
     }
