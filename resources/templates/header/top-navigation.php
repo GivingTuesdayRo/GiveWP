ad67170f15c338397edcbde0b5cf581640147c43
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-11-02 18:32:04 +0200

    make top nav optional

--- resources/templates/header/top-navigation.php
+++ resources/templates/header/top-navigation.php
@@ -1,11 +1,18 @@
+<?php
+$topMenu = wp_nav_menu([
+    'echo' => false,
+    'fallback_cb' => '__return_false',
+    'theme_location' => 'top-navigation',
+    'menu_id' => 'top-links-menu',
+    'menu_class' => 'nav navbar-nav ml-auto',
+    'container_class' => 'menu-new-top-links-container d-flex',
+    'walker' => new \GivingTuesdayWp\Library\Nav\BootstrapWalker(),
+]);
+if (empty ($menu)) {
+    return;
+}
+?>
+
 <nav id="site-top-navigation" class="top-links-navigation d-md-block d-none ml-md-auto" role="navigation">
-    <?php
-    wp_nav_menu([
-        'theme_location' => 'top-navigation',
-        'menu_id' => 'top-links-menu',
-        'menu_class' => 'nav navbar-nav ml-auto',
-        'container_class' => 'menu-new-top-links-container d-flex',
-        'walker' => new \GivingTuesdayWp\Library\Nav\BootstrapWalker(),
-    ]);
-    ?>
+    <?php echo $topMenu; ?>
 </nav>
